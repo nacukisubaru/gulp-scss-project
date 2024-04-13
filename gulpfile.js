3d405202fe7a1af1db2c222fc729a4ef7a3fbf2a
nacukisubaru <incloudff@gmail.com>
2024-04-13 16:47:40 +0300

    копирование изображений в dist и запуск сервера

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
+const server = require('gulp-server-livereload');
 
 function includeFiles() {
     return gulp.src('./src/*.html')
+        //через prefix @@ подключение html файла
         .pipe(fileInclude({
             prefix: '@@',
             basepath: '@file'
@@ -12,10 +14,28 @@ function includeFiles() {
 }
 
 function scss() {
+    //*.scss любое название файла с расширением scss
     return gulp.src('./src/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./dist/css/'))
 }
 
+function copyImages() {
+    // ** = любое название папки 
+    // * = любое название файла с любым расширением 
+    return gulp.src('./src/img/**/*')
+        .pipe(gulp.dest('./dist/img/'))
+}
+
+function startServer() {
+    return gulp.src('./dist/')
+        .pipe(server({
+            livereload: true,
+            open: true
+        }))
+}
+
+exports.startServer = startServer;
+exports.copyImages = copyImages;
 exports.scss = scss;
 exports.includeFiles = includeFiles;
